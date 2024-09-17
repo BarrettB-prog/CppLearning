d2b7f607c17a44519da8e6ffab90d6eb135bbb35
BarrettB-prog <barrettbobilin@gmail.com>
2024-09-17 16:30:11 -0400

    cases and static cast

--- cases.cpp
+++ cases.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
@@ -13,7 +5,7 @@ using namespace std;
 int main()
 {
     
-    /*
+    
     int N,D;
     cout << "enter a numerator: ";
     cin >> N;
@@ -32,9 +24,7 @@ int main()
         cout << "cant divide by 0";
     }
     // static cast turns the varible in question into a DIFFERENT TYPE of var, just for 1 calculation
-    */
-    
-    /*
+
    
     int num1,num2;
     char symbol;
