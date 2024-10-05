0106858426af110a5c1e8b9f3a1370c3e2fdaf7b
BarrettB-prog <barrettbobilin@gmail.com>
2024-10-05 10:28:07 -0400

    hourglass header file

--- hourglass.h
+++ hourglass.h
@@ -7,7 +7,7 @@ using namespace std;
 class Hourglass
 {
    public:
-    Hourglass();                // when run without params, will set to defualt values seen below. Set in .cpp file
+    Hourglass();                // when run without params, will set to default values seen below. Set in .cpp file
     Hourglass(int S = 1, char BC = '#', char FC = '*');     // S = size of hourglass, BC = BorderChar, FC = FillChar
     
     void Draw() const;
@@ -17,7 +17,7 @@ class Hourglass
     void SetFill(char FillChar);
     
     int GetSize() const;        //return Size
-    int GetPerimeter() const;   //idk what this does
+    int GetPerimeter() const;   
     double GetArea() const;     // size x size = Area. return that
     
     int Grow();
@@ -26,7 +26,7 @@ class Hourglass
     void Summary() const;
 
    private:
-    int Size;      //triangles involved are equilatateral so only 1 side length is needed
+    int Size;      //triangles involved are equilateral so only 1 side length is needed
     char BorderChar;
     char FillChar;
     
